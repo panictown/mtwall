eb367941c7dbbd7ba14940ee53518b9444d01985
y <sanlyosu@gmail.com>
2022-05-29 20:37:31 +0800

    fix(commentsModel): versionKey false

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  comment: {
-    type: String,
-    required: [true, "請輸入留言內容"],
+const commentSchema = new mongoose.Schema(
+  {
+    comment: {
+      type: String,
+      required: [true, "請輸入留言內容"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "user",
+      required: [true, "請輸入 User ID"],
+    },
+    post: {
+      type: mongoose.Schema.ObjectId,
+      ref: "post",
+      required: [true, "請輸入 Post ID"],
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: "user",
-    required: [true, "請輸入 User ID"],
-  },
-  post: {
-    type: mongoose.Schema.ObjectId,
-    ref: "post",
-    required: [true, "請輸入 Post ID"],
-  },
-});
+  { versionKey: false }
+);
 
 // mongoose 前置器 pre hook, 有用到 find 開頭語法時觸發
 commentSchema.pre(/^find/, function (next) {
